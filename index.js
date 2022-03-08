4ee719cdc3136726bf17941c2a57a37ec266ab79
nmnn33 <tommy123tran@gmail.com>
2022-03-08 21:22:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//Moduuleja
+const PORT = process.env.PORT || 5000; //Portti
+var http = require("http");
+var express = require("express");
+var app = express();    //express-moduuli
+
+//Polut
+
+app.use(express.static("./edulab"));   //HTML-sivu expressin kautta, hakee juuri hakemistosta
+
+app.get("/guestbook", function (req, res) {
+    res.send("guest!");
+});
+
+app.get("/newmessage", function (req, res) {
+    res.send("messa!");
+});
+
+app.get("/ajaxmessage", function (req, res) {
+    res.send("ajax!");
+});
+
+//Web-palvelin
+app.listen(PORT, function () {
+    console.log("Portti 5000 avattu, serveri pystyssä");
+})
+
