9dd0579016cb7e74debde9d4a94a733e04352180
nmnn33 <tommy123tran@gmail.com>
2022-03-08 23:04:02 +0200

    9

--- index.js
+++ index.js
@@ -7,22 +7,26 @@ var path = require('path');
 
 //Polut
 
+//kun url on '/' eli default
 app.use(express.static(path.join(__dirname, 'public')));
 
+//JSON-tiedosto, joka näytetään html tauluna
 app.get("/guestbook", function (req, res) {
-    res.send("guest!");
+    res.sendFile(__dirname + '/public/testi.html')
 });
 
+//FORM-tyyppinen sivu, joka tallentaa JSON-tiedostoon
 app.get("/newmessage", function (req, res) {
     res.send("messa!");
 });
 
+//ajax-pyyntö backendiin ja sitten tulostus sivulle.
 app.get("/ajaxmessage", function (req, res) {
     res.send("ajax!");
 });
 
 //Web-palvelin
 app.listen(PORT, function () {
-    console.log("Portti 8083 avattu, serveri pystyssä");
+    console.log("Portti avattu, serveri pystyssä: " + PORT);
 });
 
