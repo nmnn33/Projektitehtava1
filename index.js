86186d83cdbb521376903e943aad8c1e93ec202a
nmnn33 <tommy123tran@gmail.com>
2022-03-09 00:01:41 +0200

    alfa

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //Moduuleja
 const PORT = process.env.PORT || 8083; //Portti
 var http = require("http");
+var fs = require('fs');
 var express = require("express");
 var app = express();    //express-moduuli
 var path = require('path');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Polut
 
@@ -12,7 +16,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //JSON-tiedosto, joka näytetään html tauluna
 app.get("/guestbook", function (req, res) {
-    res.sendFile(__dirname + '/public/testi.html')
+    //index.js kautta tehty taulu, ei tarve, sillä html sisältää
+    /*var data = require('./guestbookdata.json'); //json taulu
+    var taulu = '<table border="2px">';
+    for (var i = 0; i < data.length; i++) {
+        taulu += '<tr>' +
+            '<td>' + data[i].id + '</td>' +
+            '<td>' + data[i].username + '</td>' +
+            '<td>' + data[i].country + '</td>' +
+            '<td>' + data[i].date + '</td>' +
+            '<td>' + data[i].message + '</td>' +
+            '<tr>';
+    }
+    taulu += '</table>'
+    res.send(taulu);*/
+    res.sendFile(__dirname + '/public/guestbook.html');
 });
 
 //FORM-tyyppinen sivu, joka tallentaa JSON-tiedostoon
